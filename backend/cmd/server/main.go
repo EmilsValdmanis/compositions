e2e95199ab42ebbe770584352445a9e014d12568
Emīls Valdmanis <valdmanis71@gmail.com>
2026-05-08 20:40:55 +0300

    feat: implements basic web socket connection server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,12 +1,65 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 
-	"github.com/EmilsValdmanis/compositions/backend/internal/game"
+	"github.com/gorilla/websocket"
+	// "github.com/EmilsValdmanis/compositions/backend/internal/game"
 )
 
+type Message struct {
+	Type string `json:"type"`
+	Data string `json:"data"`
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleWS(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	log.Println("client connected:", conn.LocalAddr())
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("client disconnected")
+			break
+		}
+
+		var msg Message
+
+		if err := json.Unmarshal(message, &msg); err != nil {
+			log.Println("invalid json")
+			continue
+		}
+
+		switch msg.Type {
+		case "ping":
+			response := Message{
+				Type: "pong",
+				Data: "hellow from server",
+			}
+			conn.WriteJSON(response)
+		}
+	}
+}
+
 func main() {
-	gs := game.NewGameState()
-	fmt.Println(gs)
+	http.HandleFunc("/ws", handleWS)
+	log.Println("server running on :8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/EmilsValdmanis/compositions
 go 1.26.2
 
 require github.com/google/uuid v1.6.0
+
+require github.com/gorilla/websocket v1.5.3 // indirect
